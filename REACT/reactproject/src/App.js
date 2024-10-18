5ec208672db5bf21259c4775ab0d873ddd7264d7
Raj <gowtham.raj@biworldwide.com>
2024-10-18 17:35:21 +0530

    commiting the React Code

--- REACT/reactproject/src/App.js
+++ REACT/reactproject/src/App.js
@@ -1,9 +1,315 @@
-import { render } from '@testing-library/react';
+
 import './App.css';
 import './Child3.css';
 import {Component} from 'react'
+import React,{useRef} from 'react';
+
+export default function App(){
+
+  const myRef=useRef();
+  const handleClick=()=>{
+    myRef.current.focus()
+  }
+  return(
+    <div>
+      <input ref={myRef} type='text'/>
+      <button onClick={handleClick}>Focus Input</button>
+    </div>
+  )
+
+}
+
+/*----------------------------------useEffect
+export default function App (){
+  const[count,setValue]=useState(0)
+  
+  
+
+  useEffect(()=>{
+    document.getElementById('ResultDiv').innerHTML=`you clicked ${count} times`;
+  })
+    return(
+      <>
+      <div className="ResultDiv">
+        <br></br>
+        <button onClick={()=>setValue(count+1)}></button>
+        <br></br>
+        Count{this.state.count}
+      </div>
+      </>
+    )
+  
+}
 
 
+/* component Did mount---------------------------------
+export default class App extends Component(){
+  constructor(){
+    super();
+    this.state={
+      count:0
+    }
+  }
+
+  componentDidMount(){
+    document.getElementById('ResultDiv').innerHTML="Component did mount Phase : Count is  "+this.state.count;
+  }
+  render(){
+    return(
+      <>
+      <div className="App">
+        <br></br>
+        <button onClick={()=>this.setState({count:this.state.count+1})}>Click ME</button>
+        <br></br>
+        Count{this.state.count}
+      </div>
+      </>
+    )
+  }
+
+}
+
+/*----------------------uState---------------------------------------
+export default function App(){
+  const[name,setName]=useState(""); //setting value for the name use setstate  
+  
+  
+
+  return (
+    <div className='App'>
+      <input type='text'  value={name} onChange={(e)=>setName(e.target.value)}/>
+      <h2>welcome {name}</h2>
+    </div>
+
+  )
+}
+
+
+
+/*-------------------------------use State-----------------
+export default function App(){
+ const  [count, setCount] = useState(0); // use state method to declare the value for count 
+ const increment  = () => {setCount(count+1)}
+const decrement  = () => {setCount(count-1)}
+
+
+
+  return (
+    <div className='App'>
+      <button onClick={()=>increment()}>Increment</button>
+      <button onClick={()=>decrement()}>Decrement</button>
+      <h2>{count}</h2>
+    </div>
+
+  )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*------------------------------------find even or Odd
+class App extends Component {
+  constructor() {
+    super();
+    
+    this.state = {
+      userNumber: '', 
+      result: ''      
+    };
+  }
+
+
+  inputChange = (event) => {
+    this.setState({ userNumber: event.target.value });
+  };
+
+
+  checkEvenOrOdd = () => {
+    const { userNumber } = this.state;
+    const number = parseInt(userNumber, 10); 
+
+    if ((number)) {
+     
+      const result = number % 2 === 0 ? `${number} is Even` : `${number} is Odd`; //ternary operator
+      this.setState({ result });
+    } else {
+      this.setState({ result: 'Please enter a valid number' });
+    }
+  };
+
+  render() {
+    const { userNumber, result } = this.state;
+
+    return (
+      <div>
+        <input type="number" value={this.state.userNumber} onChange={this.inputChange}/>
+
+      
+        <button onClick={this.checkEvenOrOdd}>Check</button>
+
+        <p>{result}</p>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
+
+/*------------------------------------AND Conditional Operator---------------------------
+export default function App(props)
+{ 
+  let n  = props.input;
+  return(
+    <>
+  {n%2===0 && <Even input={n}/>}
+  {n%2!==0 && <Odd input={n}/>}
+    </>
+)
+
+}
+
+function Odd(props){
+  return(
+    <b>{props.input} is Odd</b>
+  )
+}
+
+function Even(props){
+  return(
+    <b>{props.input} is Even</b>
+  )
+}
+
+/*-----------------------------------if Statement------------------------------
+export default function App(props)
+{ 
+  let n  = props.input;
+  if(n%2==0)
+      return <Even input={n}></Even>
+  else
+    return<Odd input={n}></Odd>
+
+}
+
+function Odd(props){
+  return(
+    <b>{props.input} is Odd</b>
+  )
+}
+
+function Even(props){
+  return(
+    <b>{props.input} is Even</b>
+  )
+}
+
+/*------------------------------------------------user and Password------------------------
+class App extends Component{
+  constructor(){
+    super();
+    this.state={
+      falg:false,
+      uid:"",
+      upass:"",
+      uidError:"",
+      upassError:""
+    }
+  }
+  validateUid =(uid)=>{
+    let re= /^[0-9]+$/;  //regular expression
+    if(uid==='')
+      return "Id is required "
+    else if(!re.test(uid)){
+      return "Ivalid ID"
+    }
+    else{
+      return null
+    }
+  }
+  
+  validateUpass=(upass)=>{
+    let re=/^[A-Z,a-z]+$/;
+    if(upass==='')
+      return "Password is required"
+    else if(!re.test(upass)){
+      return "Invaild password"
+    }
+    else{
+      return null
+    }
+  }
+  changeId = (e) =>{
+    this.setState({uid:e.target.value})
+    let error=this.validateUid(this.state.uid)
+  this.setState({uidError:error})
+  
+  }
+  changePass =(e)=>{
+    this.setState({upass:e.target.value})
+    let error=this.validateUpass(this.state.upass)
+    this.setState({upassError:error})
+  }
+  getResult = (e) => {
+    e.preventDefault();
+    this.setState({flag:true})
+  
+    let error=this.validateUid(this.state.uid)
+    this.setState({eidError:error})
+  
+    let error1=this.validateUpass(this.state.upass)
+    this.setState({enameError:error1})
+  
+  
+    if(!error&&!error1){
+      this.setState({flag:true}); //validating all error is null
+    }
+  }
+  
+  render(){
+    return(
+      <div>
+<form>
+<label for="first">Enter User Id:</label>
+<input type='text' value={this.state.uid} onChange={this.changeId}></input><br></br><br></br>
+<br></br><div><font color='red'><b>{this.state.uidError}</b></font></div> 
+
+<label for="second">Enter User Password:</label>
+<input type='text' value={this.state.upass} onChange={this.changePass}></input><br></br>
+<br></br><div><font color='red'><b>{this.state.upassError}</b></font></div>
+
+
+<button onClick={this.getResult}>Submit</button>
+</form>
+          {this.state.flag? // check the condition to execute
+<div>
+<br></br>SuccessFull.!!
+</div>
+          :''
+          }
+</div>
+  );
+}
+}
+export default App;
+      
+
+/*
 class App extends Component{
 
 constructor() {
@@ -12,25 +318,100 @@ constructor() {
       flag:false,
       eid: "",
       ename: "",
-      esalary: ""
-      ,employee:""
+      esalary: "",
+      employee:"",
+      eidError:"",
+      enameError:"",
+      esalaryError:""
   }
 }
-GetResult = (e) => {
+validateEid =(eid)=>{
+  let re= /^[0-9]+$/;  //regular expression
+  if(eid==='')
+    return "Id is required "
+  else if(!re.test(eid)){
+    return "Ivalid ID"
+  }
+  else{
+    return null
+  }
+}
+
+validateEname =(ename)=>{
+  let re=/^[A-Z,a-z]+$/;
+  if(ename==='')
+    return "Name is required"
+  else if(!re.test(ename)){
+    return "Invaild Name"
+  }
+  else{
+    return null
+  }
+}
+
+
+validateSalary =(esalary)=>{
+  let re=/^[0-9]+$/;
+  if(esalary==='')
+    return "salary is required"
+  else if(!re.test(esalary)){
+    return "Invaild salary"
+  }
+  else{
+    return null
+  }
+}
+changeId = (e) =>{
+  this.setState({eid:e.target.value})
+  let error=this.validateEid(this.state.eid)
+this.setState({eidError:error})
+
+}
+changeName =(e)=>{
+  this.setState({ename:e.target.value})
+  let error=this.validateEname(this.state.ename)
+  this.setState({enameError:error})
+}
+changeSalary=(e)=>{
+  this.setState({esalary:e.target.value})
+  let error=this.validateSalary(this.state.esalaryError)
+  this.setState({esalaryError:error})
+}
+getResult = (e) => {
   e.preventDefault();
   this.setState({flag:true})
+
+  let error=this.validateEid(this.state.eid)
+  this.setState({eidError:error})
+
+  let error1=this.validateEname(this.state.ename)
+  this.setState({enameError:error1})
+
+  let error2=this.validateSalary(this.state.esalary)
+  this.setState({esalaryError:error2})
+
+  if(!error&&!error1&&error2){
+    this.setState({flag:true}); //validating all error is null
+  }
 }
 render() {
   return (
 <div>
 <form>
 <label for="first">Enter Employee Id:</label>
-<input type='text' value={this.state.eid} onChange={(e) => this.setState({ eid: e.target.value })}></input><br></br><br></br>
+<input type='text' value={this.state.eid} onChange={this.changeId}></input><br></br><br></br>
+<br></br><div><font color='red'><b>{this.state.eidError}</b></font></div> 
+
 <label for="second">Enter Employee Name:</label>
-<input type='text' value={this.state.ename} onChange={(e) => this.setState({ ename: e.target.value })}></input><br></br><br></br>
+<input type='text' value={this.state.ename} onChange={this.changeName}></input><br></br>
+<br></br><div><font color='red'><b>{this.state.enameError}</b></font></div>
+
+
 <label for="third">Enter Employee Salary:</label>
-<input type='text' value={this.state.esalary} onChange={(e) => this.setState({ esalary: e.target.value })}></input><br></br><br></br>
-<button onClick={this.GetResult}>Submit</button>
+<input type='text' value={this.state.esalary} onChange={this.changeSalary}></input><br></br><br></br>
+<br></br><div><font color='red'><b>{this.state.esalaryError}</b></font></div>
+
+<button onClick={this.getResult}>Submit</button>
 </form>
           {this.state.flag ? // check the condition to execute
 <div>
